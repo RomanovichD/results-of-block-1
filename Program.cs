708b77702b575c93ade33b5f89d838c597c5acb8
Dmitry <romanovichd.hr@gmail.com>
2022-10-02 15:18:30 +0300

    add second array

--- Program.cs
+++ Program.cs
@@ -10,18 +10,44 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+// Создаем первый массив, считаем сколько элементов меньше 4
 Console.WriteLine("Введите размерность массива");
 int n = Convert.ToInt32(Console.ReadLine());
-string[] firstArr = new string [n];
-int i = 0;
-while (i < n)
+string[] firstArr = new string[n];
+int FirstIndex = 0;
+int count = 0;
+while (FirstIndex < n)
 {
-    Console.WriteLine($"Введите {i+1} элемент массива");
-    firstArr[i] =  Convert.ToString(Console.ReadLine());
-    i++;
+    Console.WriteLine($"Введите {FirstIndex + 1} элемент массива");
+    firstArr[FirstIndex] = Convert.ToString(Console.ReadLine());
+    if (firstArr[FirstIndex].Length < 4) count++;
+    FirstIndex++;
 }
 
-for (i=0; i < n; i++)
+// Печатаем первый массив
+Console.Write("Первый массив:");
+for (FirstIndex = 0; FirstIndex < n; FirstIndex++)
 {
-    Console.Write($"{firstArr[i]} ");
+    Console.Write($" {firstArr[FirstIndex]}");
 }
+Console.WriteLine();
+
+//Создаем и заполняем второй массив
+string[] secondArr = new string[count];
+int SecondIndex = 0;
+for (FirstIndex = 0; FirstIndex < n; FirstIndex++)
+{
+    if (firstArr[FirstIndex].Length < 4)
+    {
+        secondArr[SecondIndex] = firstArr[FirstIndex];
+        SecondIndex++;
+    }
+}
+
+//Печатаем второй массив
+Console.Write("Второй массив:");
+for (SecondIndex = 0; SecondIndex < count; SecondIndex++)
+{
+    Console.Write($" {secondArr[SecondIndex]}");
+}
+Console.WriteLine();
